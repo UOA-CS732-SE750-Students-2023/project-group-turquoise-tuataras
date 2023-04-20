8f1c88f5044f1f715d0d75989ccf09974dc5b60f
Benjamin Goh <benjaminjzgoh@gmail.com>
2023-04-20 16:50:46 +1200

    Post Rating endpoint created

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -28,4 +28,25 @@ router.post('/recipe/:spoonacularId/comment', async (req, res) => {
     }
 });
 
+router.post('/recipe/:spoonacularId/rating', async (req, res) => {
+    const { spoonacularId } = req.params;
+    const rating = req.body.rating;
+    const recipe = await Recipe.findOne({'spoonacularId': spoonacularId});
+    if (recipe){
+        const newCalculatedRating = (recipe.rating.rating*recipe.rating.numberOfRatings + rating)/(recipe.rating.numberOfRatings + 1);
+        const newRating = {
+            rating: newCalculatedRating,
+            numberOfRatings: recipe.rating.numberOfRatings+1
+        };
+        recipe.rating = newRating;
+        await recipe.save();
+        res.status(201).json({
+            "message": "New rating: " + newCalculatedRating
+        });
+    }
+    else{
+        res.status(404).json({"message": "Recipe with that spoonacular ID not found"});
+    }
+});
+
 export default router;
