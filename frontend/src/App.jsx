a3f1dd48e36346600cb4d6eacca9e49ce611656f
unknown <yuhoklai168@gmail.com>
2023-05-01 17:45:44 +1200

    frontend auth
    completed signup, login fetch
    log in display username

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,82 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import Navbar from './Navbar'
-import Auth from './Auth';
+import SignUp from './SignUp';
+import Login from './Login';
 import AdvanceSearch from './AdvanceSearch';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 function App() {
+  const [user, setUser] = useState("");
 
-  //handle showing auth modal
-  const [authModalShow, setauthModalShow] = useState(false);
+  const [signUpModalShow, setSignUpModalShow] = useState(false);
 
-  const handleAuthModalClose = () => setauthModalShow(false);
-  const handleAuthModalShow = () => setauthModalShow(true);
+  const handleSignUpModalClose = () => setSignUpModalShow(false);
+  const handleSignUpModalShow = () => setSignUpModalShow(true);
 
+  const [logInModalShow, setLogInModalShow] = useState(false);
+
+  const handleLogInModalClose = () => setLogInModalShow(false);
+  const handleLogInModalShow = () => setLogInModalShow(true);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (username, password) => {
+      axios.post(`${API_BASE_URL}/api/login`,{
+        username,
+        password
+      })
+      .then((response) => {
+        if(response.status === 200) {
+          localStorage.setItem("user", JSON.stringify(username));
+          setIsLoggedIn(true);
+          setLogInModalShow(false);
+          setUser(username);
+        } 
+      })
+    console.log(username);
+    console.log(password);
+  };
+
+  const handleSignup = (username, password) => {
+    axios.post(`${API_BASE_URL}/api/signup`,{
+      username,
+      password
+    })
+    .then((response) => {
+      if(response.status === 200) {
+        localStorage.setItem("user", JSON.stringify(username));
+        setSignUpModalShow(false);
+      } 
+    })
+  console.log(username);
+  console.log(password);
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser("");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+    if (currentUser) {
+      setUser(currentUser);
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   return (
     <BrowserRouter>
       <div>
-        <Navbar onShow={handleAuthModalShow}/>
-        <Auth show={authModalShow} onHide={handleAuthModalClose}/>
+        <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} onSignUpShow={handleSignUpModalShow} onLogInShow={handleLogInModalShow} user={user}/>
+        <SignUp show={signUpModalShow} onHide={handleSignUpModalClose} handleSignup={handleSignup}/>
+        <Login show={logInModalShow} onHide={handleLogInModalClose} handleLogin={handleLogin}/>
         <Routes>
           <Route path="/advance-search"
             element={<AdvanceSearch/>}/>
