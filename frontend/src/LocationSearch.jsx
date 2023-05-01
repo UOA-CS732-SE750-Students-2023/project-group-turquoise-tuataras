4c9946ac6a6ac5d71b43a1bff5e72e427a74276d
Benjamin Goh <benjaminjzgoh@gmail.com>
2023-05-01 17:01:48 +1200

    Map loads based on location

--- frontend/src/LocationSearch.jsx
+++ frontend/src/LocationSearch.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '800px',
+  height: '800px'
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
 function LocationSearch() {
-    navigator.geolocation.getCurrentPosition(position => {
-        const { latitude, longitude } = position.coords;
-        // Show a map centered at latitude / longitude.
-    });
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+    })
     return (
       <LoadScript
         googleMapsApiKey="AIzaSyCLEu_YtuPwWib7dyJSbRWzP0T2KvfAvpc"
       >
         <GoogleMap
           mapContainerStyle={containerStyle}
-          center={center}
+          center={
+                {
+                    lat: lat,
+                    lng: lng
+                }
+            }
           zoom={10}
         >
           { /* Child components, such as markers, info windows, etc. */ }
