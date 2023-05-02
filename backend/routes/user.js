68f6ae6b0b3e1150faa6064f1ab028d9d46ca7bf
Cameron Nathan <cmrnnathan@gmail.com>
2023-05-02 12:21:00 +1200

    feat: adding token to signup endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,13 @@ import express from 'express';
 import {
     signupUser,
     loginUser } from '../database/user-dao';
+import jwt from 'jsonwebtoken'
 
 const router = express.Router();
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' });
+}
 
 //login route
 router.post('/login', async (req, res) => {
@@ -17,7 +22,10 @@ router.post('/signup', async (req, res) => {
     try {
         const user = await signupUser(username, password);
 
-        res.status(201).json({username, user})
+        //create token
+        const token = createToken(user._id)
+
+        res.status(201).json({username, token})
     } catch (error) {
         res.status(400).json({error: error.message})
     }
