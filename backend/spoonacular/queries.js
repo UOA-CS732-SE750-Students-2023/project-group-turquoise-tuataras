0424737bc4b22d7c5b50dada53ea6658f67242b7
Yasith-Banuka <47107056+Yasith-Banuka@users.noreply.github.com>
2023-05-04 10:44:58 +1200

    fix: async issue with retrieving data

--- backend/spoonacular/queries.js
+++ backend/spoonacular/queries.js
@@ -6,55 +6,44 @@ dotenv.config();
 const SPOONACULAR_URL = process.env.SPOONACULAR_URL ?? '';
 const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY ?? '';
 
-export function searchRecipes(query) {
-    axios.get(`${SPOONACULAR_URL}/complexSearch`, {
+export async function searchRecipes(query) {
+    const response = await axios.get(`${SPOONACULAR_URL}/complexSearch`, {
         params: {
             apiKey: SPOONACULAR_API_KEY,
             ...query
         }
     })
-        .then(response => {
-          return response.data.results;
-    })
-        .catch(error => {
-            console.log(error);
-    })
+    return response.data.results;
 }
 
-export function getRecipe(id) {
+export async function getRecipeBySpoonacularId(id) {
 
-
-    axios.get(`${SPOONACULAR_URL}/${id}/information`, {
+    const response = await axios.get(`${SPOONACULAR_URL}/${id}/information`, {
         params: {
             apiKey: SPOONACULAR_API_KEY,
             includeNutrition: true,
         }
     })
-        .then(response => {
-            const recipe = response.data;
-            const returnRecipe = {
-                spoonacularId: recipe.id,
-                types: {
-                    vegetarian: recipe.vegetarian,
-                    vegan: recipe.vegan,
-                    glutenFree: recipe.glutenFree,
-                    dairyFree: recipe.glutenFree
-                },
-                ingredients: parseIngredients(recipe.extendedIngredients),
-                title: recipe.title,
-                image: recipe.image,
-                readyInMinutes: recipe.readyInMinutes,
-                servings: recipe.servings,
-                nutrition: recipe.nutrition.nutrients,
-                summary: recipe.summary,
-                cuisines: recipe.cuisines,
-                instructions: recipe.analyzedInstructions[0].steps.map(step => step.step)
-            }
-            return returnRecipe;
-        })
-        .catch(error => {
-            console.log(error);
-        })
+    const recipe = response.data;
+    const returnRecipe = {
+        spoonacularId: recipe.id,
+        types: {
+            vegetarian: recipe.vegetarian,
+            vegan: recipe.vegan,
+            glutenFree: recipe.glutenFree,
+            dairyFree: recipe.glutenFree
+        },
+        ingredients: parseIngredients(recipe.extendedIngredients),
+        title: recipe.title,
+        image: recipe.image,
+        readyInMinutes: recipe.readyInMinutes,
+        servings: recipe.servings,
+        nutrition: recipe.nutrition.nutrients,
+        summary: recipe.summary,
+        cuisines: recipe.cuisines,
+        instructions: recipe.analyzedInstructions[0].steps.map(step => step.step)
+    }
+    return returnRecipe;
 }
 
 function parseIngredients(extendedIngredients) {
