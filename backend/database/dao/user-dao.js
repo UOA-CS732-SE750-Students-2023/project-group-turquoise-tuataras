1abf48af79697dfaea151ae52706d1ccbfeb3b7b
Yasith-Banuka <47107056+Yasith-Banuka@users.noreply.github.com>
2023-05-03 12:12:26 +1200

    refac: extract dao logic of users endpoints

--- backend/database/dao/user-dao.js
+++ backend/database/dao/user-dao.js
@@ -1,3 +1,5 @@
+import {User} from "../schema/user-schema.js";
+
 export function getIntolerances(userId) {
     User.findById(userId, (err, user) => {
         if (err) {
@@ -5,4 +7,18 @@ export function getIntolerances(userId) {
         }
         return user.intolerances;
     });
+}
+
+export async function setUserIntolerances(userId, intolerances) {
+    const user = await User.findById(userId);
+    if (user) {
+        user.intolerances = intolerances;
+        user.save().then((user) => {
+            return Promise.resolve();
+        }).catch((err) => {
+            return Promise.reject(err);
+        });
+    } else {
+        return Promise.reject("User with userId " + userId + " not found");
+    }
 }
