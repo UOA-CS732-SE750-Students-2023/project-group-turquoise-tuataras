9282d96abb8c4257478ad06fa4bcfe2db9ecfd83
Benjamin Goh <benjaminjzgoh@gmail.com>
2023-05-05 22:48:18 +1200

    Fixed recipe schema test
    Test now looks at all fields in a recipe. Required a slight change to the recipe schema file.

--- backend/database/_tests_/schema.test.js
+++ backend/database/_tests_/schema.test.js
@@ -72,6 +72,42 @@ it('get all users', async () => {
 it('get a specific recipe', async () => {
     const recipeFromDB = await Recipe.findOne({spoonacularId: 650181});
     expect(recipeFromDB.spoonacularId).toBe(650181);
+    expect(recipeFromDB.types).toEqual(recipe.types);
+    const ingredients = recipeFromDB.ingredients;
+    var ingredientsNoId = [];
+    for (var i = 0; i < ingredients.length; i++){
+        ingredientsNoId[i] = {name: ingredients[i].name, amount: ingredients[i].amount, unit: ingredients[i].unit}
+    }
+    expect(ingredientsNoId).toEqual(recipe.ingredients);
+    expect(recipeFromDB.title).toBe(recipe.title);
+    expect(recipeFromDB.image).toBe(recipe.image);
+    expect(recipeFromDB.readyInMinutes).toBe(recipe.readyInMinutes);
+    expect(recipeFromDB.servings).toBe(recipe.servings);
+    const nutrition = recipeFromDB.nutrition;
+    var nutritionNoId = [];
+    for (var i = 0; i < nutrition.length; i++){
+        nutritionNoId[i] = {
+            name: nutrition[i].name,
+            amount: nutrition[i].amount, 
+            percentOfDailyNeeds: nutrition[i].percentOfDailyNeeds, 
+            unit: nutrition[i].unit
+        }
+    }    
+    expect(nutritionNoId).toEqual(recipe.nutrition);
+    expect(recipeFromDB.summary).toBe(recipe.summary);
+    expect(recipeFromDB.cuisines).toEqual(recipe.cuisines);
+    expect(recipeFromDB.instructions).toEqual(recipe.instructions);
+    expect(recipeFromDB.rating).toEqual(recipe.rating);
+    const comments = recipeFromDB.comments;
+    var commentsNoId = [];
+    for (var i = 0; i < comments.length; i++){
+        commentsNoId[i] = {
+            username: comments[i].username,
+            comment: comments[i].comment,
+            date: comments[i].date.toISOString().split('T')[0]
+        }
+    } 
+    expect(commentsNoId).toEqual(recipe.comments);
 });
 
 it('get a specific user', async () => {
@@ -81,7 +117,6 @@ it('get a specific user', async () => {
 
 it('get a day meal plan', async () => {
     const mealPlan = await DayMealPlan.findOne({dateTime:'2025-10-06'}).populate('user').populate('recipe');
-    console.log(mealPlan);
     expect(mealPlan.user.username).toBe('root');
     expect(mealPlan.recipe[0].spoonacularId).toBe(650181);
 });
