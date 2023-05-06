31cc7dfc30ae8e054706a9165e51c3818c1763fe
hzyhzyq <53165831+hzyhzyq@users.noreply.github.com>
2023-05-07 01:57:44 +1200

    feat: meal plan page version 3 first complete version

--- frontend/src/MealSchedule.jsx
+++ frontend/src/MealSchedule.jsx
@@ -3,7 +3,7 @@ import style from "./MealSchedule.module.css"
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import {ListGroup, ListGroupItem} from "react-bootstrap";
+import {ListGroup, ListGroupItem, Table} from "react-bootstrap";
 import * as url from "url";
 
 let inputValue = "-1";
@@ -13,13 +13,21 @@ function inputValueChangeHandler(event) {
 }
 
 function MealSchedule({isLoggedIn, user}) {
-    // JUST FOR TEST
+    // JUST FOR TEST, DELETE IN FINAL VERSION
     isLoggedIn = true;
-    user = '643e631893f3ed7ee9a445df';
+    user = '64565d5a79fb1a04100f27c2';
     // constants
     const WEEKS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const MONTH = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     let [tag, setTag] = useState(false);
+    //open and close schedule
+    let refObject = useRef();
+    const funClose = () => {
+        refObject.current.style.display = "none";
+    }
+    const funOpen = () => {
+        refObject.current.style.display = "block";
+    }
     // function add
     const funAdd = () => {
         //send request
@@ -46,7 +54,6 @@ function MealSchedule({isLoggedIn, user}) {
                 }
             }
         }
-
         setTag(!tag);
     }
     // function delete
@@ -125,64 +132,83 @@ function MealSchedule({isLoggedIn, user}) {
         }
     }, [tag]);
     return (<div>
-            {isLoggedIn ? <div>
-                <div className={style.meal_schedule_title}>
-                    <div className={style.meal_schedule_time_picker}></div>
-                    <ul>
-                        {dateArray.map((item, index) => <li style={{color: index === select ? '#dc3545' : 'black'}}
-                                                            key={index} onClick={() => {
-                            setSelect(index);
-                            setTag(!tag);
-                        }}>
-                            <a>{index === 0 ? 'Today' : WEEKS[item.getDay()]}<br/>{item.getDate()} {MONTH[item.getMonth()]}
-                            </a></li>)}
-                    </ul>
+        {isLoggedIn ? <div>
+            <div className={style.meal_schedule_title}>
+                <div className={style.meal_schedule_time_picker}></div>
+                <ul>
+                    {dateArray.map((item, index) => <li style={{color: index === select ? 'red' : 'black'}}
+                                                        key={index} onClick={() => {
+                        setSelect(index);
+                        setTag(!tag);
+                    }}>
+                        <a>{index === 0 ? 'Today' : WEEKS[item.getDay()]}<br/>{item.getDate()} {MONTH[item.getMonth()]}
+                        </a></li>)}
+                </ul>
 
+            </div>
+            <div className={style.meal_schedule_content}>
+                <div className={style.meal_schedule_content_date}>
+                    <a>{dateArray[select].getDate()}</a>
+                    <div
+                        className={style.meal_schedule_content_date_month}>{MONTH[dateArray[select].getMonth()]}</div>
                 </div>
-                <div className={style.meal_schedule_content}>
-                    <div className={style.meal_schedule_content_date}>
-                        <a>{dateArray[select].getDate()}</a>
-                        <div
-                            className={style.meal_schedule_content_date_month}>{MONTH[dateArray[select].getMonth()]}</div>
+                <div className={style.meal_schedule_button}>Shopping List</div>
+                <div className={style.meal_schedule_button} style={{top: "280px"}} onClick={funOpen}>Meal Schedule</div>
+                <div className={style.meal_schedule_content_table}>
+                    <div className={style.meal_schedule_items_group}>
+                        <ListGroup as="ul">
+                            {data.get(dateArray[select].getDate()) && Array.from(data.get(dateArray[select].getDate()).values()).map((item, index) =>
+                                <ListGroupItem as="li" key={index} className={style.meal_schedule_items}>
+                                    {item.title}
+                                    <Button variant="outline-danger"
+                                            className={style.meal_schedule_delete_button}
+                                            onClick={(e) => {
+                                                funDelete(item)
+                                            }}>DELETE</Button>
+                                </ListGroupItem>)}
+                            {data.get(dateArray[select].getDate()) && Array.from(data.get(dateArray[select].getDate()).values()).length === 0 &&
+                                <div className={style.meal_schedule_items_none}>Add recipes to your schedule</div>}
+                        </ListGroup>
                     </div>
-                    <div className={style.meal_schedule_button}>Shopping List</div>
-                    <div className={style.meal_schedule_button} style={{top: "280px"}}>Meal Schedule</div>
-                    <div className={style.meal_schedule_content_table}>
-                        <div className={style.meal_schedule_items_group}>
-                            <ListGroup as="ul">
-                                {data.get(dateArray[select].getDate()) && Array.from(data.get(dateArray[select].getDate()).values()).map((item, index) =>
-                                    <ListGroupItem as="li" key={index} className={style.meal_schedule_items}>
-                                        {item.title}
-                                        <Button variant="outline-danger"
-                                                className={style.meal_schedule_delete_button}
-                                                onClick={(e) => {
-                                                    funDelete(item)
-                                                }}>DELETE</Button>
-                                    </ListGroupItem>)}
-                                {data.get(dateArray[select].getDate()) && Array.from(data.get(dateArray[select].getDate()).values()).length === 0 &&
-                                    <div className={style.meal_schedule_items_none}>Add recipes to your schedule</div>}
-                            </ListGroup>
+                    <div style={{position: "absolute", top: "10px", width: "100%"}}>
+                        <div style={{position: "relative", display: "inline-block", width: "85%", left: "2%"}}>
+                            <Form.Select onChange={inputValueChangeHandler.bind(this)}
+                                         defaultValue="Select a recipe from saved recipes">
+                                <option value="-1">Select a recipe from saved recipes</option>
+                                {Array.from(savedRecipes.keys()).map((item, index) => !data.get(dateArray[select].getDate()).has(item) &&
+                                    <option key={index} value={item}>{savedRecipes.get(item)}</option>)}
+                            </Form.Select>
                         </div>
-                        <div style={{position: "absolute", top: "10px", width: "100%"}}>
-                            <div style={{position: "relative", display: "inline-block", width: "85%", left: "2%"}}>
-                                <Form.Select onChange={inputValueChangeHandler.bind(this)}
-                                             defaultValue="Select a recipe from saved recipes">
-                                    <option value="-1">Select a recipe from saved recipes</option>
-                                    {Array.from(savedRecipes.keys()).map((item, index) => !data.get(dateArray[select].getDate()).has(item) &&
-                                        <option key={index} value={item}>{savedRecipes.get(item)}</option>)}
-                                </Form.Select>
-                            </div>
-                            <div style={{
-                                position: "relative", display: "inline-block", width: "10%", top: "-2px", left: "3%"
-                            }}>
-                                <Button variant="outline-secondary" style={{width: "100%"}}
-                                        onClick={funAdd}>ADD</Button>
-                            </div>
+                        <div style={{
+                            position: "relative", display: "inline-block", width: "10%", top: "-2px", left: "3%"
+                        }}>
+                            <Button variant="outline-secondary" style={{width: "100%"}}
+                                    onClick={funAdd}>ADD</Button>
                         </div>
                     </div>
                 </div>
-            </div> : <div>False</div>}
-        </div>);
+            </div>
+            <div className={style.meal_schedule_show} ref={refObject}>
+                <div className={style.meal_schedule_show_content}>
+                    <div className={style.meal_schedule_show_items}>
+                        {dateArray.map((item, index) => <ListGroup className={style.meal_schedule_show_items_item}
+                                                                   key={index}>
+                            <ListGroup.Item style={{textAlign: "center", background: "rgba(0, 0, 0, 0.15)"}}>
+                                <a>{index === 0 ? 'Today' : WEEKS[item.getDay()]}<br/>{item.getDate()} {MONTH[item.getMonth()]}
+                                </a>
+                            </ListGroup.Item>
+                            {data.get(item.getDate()) && Array.from(data.get(item.getDate()).values()).map((item, index) => item &&
+                                <ListGroup.Item key={index} style={{
+                                    textAlign: "center", fontSize: "12px", background: "rgba(0, 0, 0, 0.03)"
+                                }}>{item.title}</ListGroup.Item>)}
+                        </ListGroup>)}
+                    </div>
+                    <Button variant="light" className={style.meal_schedule_show_items_bottom}
+                            onClick={funClose}>CLOSE</Button>
+                </div>
+            </div>
+        </div> : <div>False</div>}
+    </div>);
 }
 
 export default MealSchedule;
