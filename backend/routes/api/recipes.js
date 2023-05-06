e77eb50fb70c97049fb8323814085f49b7c9df46
Cameron Nathan <cmrnnathan@gmail.com>
2023-05-06 14:20:18 +1200

    feat: protecting recipes routes
    User requires JWT to access endpoints. Note that the validation isn't users specific (e.g. you can request the data of another user if you have your own valid JWT)

--- backend/routes/api/recipes.js
+++ backend/routes/api/recipes.js
@@ -2,6 +2,50 @@ import express from "express";
 import {searchRecipes, getRecipeBySpoonacularId} from "../../spoonacular/queries.js";
 import {createRecipe, getRecipeById} from "../../database/dao/recipe-dao.js";
 import {getIntolerances} from "../../database/dao/user-dao.js";
+import requireAuth from '../../middleware/requireAuth.js'
+
+const authRouter = express.Router();
+
+// Apply requireAuth middleware to all routes on the auth router
+authRouter.use(requireAuth);
+
+authRouter.post('/:spoonacularId/comment', async (req, res) => {
+    const { spoonacularId } = req.params;
+    const comment = req.body;
+    const recipe = await getRecipe(spoonacularId, true);
+    if (recipe){
+        recipe.comments.push({
+            username: comment.username,
+            comment: comment.comment
+        });
+        recipe.save();
+        res.status(201).json(comment);
+    }
+    else{
+        res.status(404).json({"message": `Recipe with spoonacular ID: ${spoonacularId} not found`});
+    }
+});
+
+authRouter.post('/:spoonacularId/rating', async (req, res) => {
+    const { spoonacularId } = req.params;
+    const rating = req.body.rating;
+    const recipe = await getRecipe(spoonacularId, true);
+    if (recipe){
+        const newCalculatedRating = (recipe.rating.rating*recipe.rating.numberOfRatings + rating)/(recipe.rating.numberOfRatings + 1);
+        const newRating = {
+            rating: newCalculatedRating,
+            numberOfRatings: recipe.rating.numberOfRatings+1
+        };
+        recipe.rating = newRating;
+        await recipe.save();
+        res.status(201).json({
+            "message": "New rating: " + newCalculatedRating
+        });
+    }
+    else{
+        res.status(404).json({"message": `Recipe with spoonacular ID: ${spoonacularId} not found`});
+    }
+});
 
 const router = express.Router();
 
@@ -31,43 +75,8 @@ router.get("/:spoonacularId", async (req, res) => {
     }
 });
 
-router.post('/:spoonacularId/comment', async (req, res) => {
-    const { spoonacularId } = req.params;
-    const comment = req.body;
-    const recipe = await getRecipe(spoonacularId, true);
-    if (recipe){
-        recipe.comments.push({
-            username: comment.username,
-            comment: comment.comment
-        });
-        recipe.save();
-        res.status(201).json(comment);
-    }
-    else{
-        res.status(404).json({"message": `Recipe with spoonacular ID: ${spoonacularId} not found`});
-    }
-});
-
-router.post('/:spoonacularId/rating', async (req, res) => {
-    const { spoonacularId } = req.params;
-    const rating = req.body.rating;
-    const recipe = await getRecipe(spoonacularId, true);
-    if (recipe){
-        const newCalculatedRating = (recipe.rating.rating*recipe.rating.numberOfRatings + rating)/(recipe.rating.numberOfRatings + 1);
-        const newRating = {
-            rating: newCalculatedRating,
-            numberOfRatings: recipe.rating.numberOfRatings+1
-        };
-        recipe.rating = newRating;
-        await recipe.save();
-        res.status(201).json({
-            "message": "New rating: " + newCalculatedRating
-        });
-    }
-    else{
-        res.status(404).json({"message": `Recipe with spoonacular ID: ${spoonacularId} not found`});
-    }
-});
+// Mount the auth router onto the main router for the routes that require authentication
+router.use(['/:spoonacularId/comment', '/:spoonacularId/rating'], authRouter);
 
 async function getRecipe(spoonacularId, createIfNotExists = false) {
     let recipe = await getRecipeById(spoonacularId);
