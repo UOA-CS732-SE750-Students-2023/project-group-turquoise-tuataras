616874e773af7bdb94735c1792b5f60819c7ed0e
ShanFanLan <slan526@aucklanduni.ac.nz>
2023-05-07 14:17:05 +1200

    0507 14:17

--- frontend/src/NutritionPie.jsx
+++ frontend/src/NutritionPie.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Pie, G2 } from '@ant-design/plots';
-import styles from './Ingredients.module.css';
+import styles from './NutritionPie.module.css';
 
 const G = G2.getEngine('canvas');
 
@@ -9,14 +9,18 @@ export default function NutritionPie({recipe}) {
 
     const data = getNutrituionData(recipe);
 
+    console.log("data = " , data)
+
     function getNutrituionData(recipe) {
 
         const PieData = [];
 
-        for (let index = 0; index < recipe.nutrition.length; index++) {
-            PieData[index] = {type:recipe.nutrition[index]["name"] , 
+        for (let index = 0; index < recipe.nutrition.length-1; index++) {
+            PieData[index] = {type:recipe.nutrition[index+1]["name"] , 
                               value: Math.floor(recipe.nutrition[index].amount)}
         };
+
+        console.log("PieData = " , PieData)
 
         return PieData;
     } 
@@ -92,7 +96,8 @@ export default function NutritionPie({recipe}) {
 
     return(
         <div style={{ width: 750 , height: 500} }>
-            <Pie {...pieconfig}/>                     
+            <Pie {...pieconfig}/>    
+            <span className={styles.Calories} >Calories: {recipe.nutrition[0].amount} cal</span>             
         </div>
     );
 }
