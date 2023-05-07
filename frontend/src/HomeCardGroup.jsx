d9580b561d42fe807d9a75a464d5e00ca36bd0c5
hzyhzyq <53165831+hzyhzyq@users.noreply.github.com>
2023-05-08 02:22:28 +1200

    feat: home page add animation

--- frontend/src/HomeCardGroup.jsx
+++ frontend/src/HomeCardGroup.jsx
@@ -9,44 +9,27 @@ function homeCardGroup(props) {
         if (id.length === 0) {
             return;
         }
-        window.location.href = "./recipe?id=" + id;
+        window.location.href = "./recipe/" + id;
     }
-    // get data from backend
-    const [data, setData] = useState([]);
-    // const url = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=1414f1ede1c14cc3b2f498b8cfad8239';
-    // const url = API_BASE_URL+'/api/complexSearch';
-    // useEffect(() => {
-    //     try {
-    //         axios.get(url + "&query=" + props.type)
-    //             .then(response => {
-    //                 let data = response.data.results;
-    //                 if (data.length > 7) {
-    //                     setData(data.slice(0, 8))
-    //                 }
-    //             });
-    //     } catch (err) {
-    //         console.error(err);
-    //     }
-    // }, []);
     return (<div>
-            {data.length === 8 ?
+            {props.data.value.length === 10 ?
                 <div className={style.card_group}>
                     <div className={style.card_group_title}
-                         style={{fontSize: "var(--card_title_" + props.index + ")"}}>{props.type}</div>
+                         style={{fontSize: "var(--card_title_" + props.index + ")"}}>{props.data.name}</div>
                     <div className={style.card_group_title_animation} style={{
                         fontSize: "var(--card_title_" + props.index + ")",
                         opacity: "var(--card_" + props.index + ")"
-                    }}>{props.type}</div>
+                    }}>{props.data.name}</div>
                     <div className={style.card_group_items} style={{opacity: "var(--card_group_" + props.index + ")"}}>
-                        {data.map((item) =>
+                        {props.data.value.map((item) =>
                             <Card className={style.card_group_card} key={item.id} onClick={() => recipe(item.id)}>
                                 <Card.Img variant="top" height="160rem" src={item.image}/>
                                 <Card.Body>
-                                    <Card.Title>{item.title.length > 17 ? item.title.slice(0, 17) + "..." : item.title}</Card.Title>
+                                    <Card.Title style={{fontSize:"16px"}}>{item.title.length > 22 ? item.title.slice(0, 22) + "..." : item.title}</Card.Title>
                                 </Card.Body>
                             </Card>)}
                     </div>
-                </div> : <div className={style.card_group_error}>Cannot get data from remote</div>}
+                </div> : <div className={style.card_group_error}>Cannot get data from back-end</div>}
         </div>
     );
 }
--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import HomeCardGroup from "./HomeCardGroup.jsx";
 import React, {useEffect, useRef, useState} from "react";
 import axios from "axios";
+import testValue from "./__test__/test.json"
 
 
 function homePage(userName) {
@@ -23,16 +24,42 @@ function homePage(userName) {
     }, []);
     // get data from backend
     const [data, setData] = useState([]);
+    const searchBox = useRef();
+    const coverBox = useRef();
     const url = 'http://localhost:3000/api/recipes/recommendations?userName=';
     useEffect(() => {
+        setTimeout(() => {
+            coverBox.current.style.animationPlayState = "running";
+            searchBox.current.style.display = "block";
+        }, 3000);
+        setTimeout(() => {
+            coverBox.current.style.display = "none";
+        }, 4000);
         try {
-            axios.get(url+userName)
+            axios.get(url + userName)
                 .then(response => {
                     let data = response.data;
-                    console.log(data)
-                });
+                    let dataArray = [];
+                    for (let propName in data) {
+                        dataArray.push({name: propName, value: data[propName]})
+                    }
+                    setData(dataArray)
+                }).catch(err => {
+                let data = testValue;
+                let dataArray = [];
+                for (let propName in data) {
+                    dataArray.push({name: propName, value: data[propName]})
+                }
+                setData(dataArray)
+            });
         } catch (err) {
             console.error(err);
+            let data = testValue;
+            let dataArray = [];
+            for (let propName in data) {
+                dataArray.push({name: propName, value: data[propName]})
+            }
+            setData(dataArray)
         }
     }, []);
 
@@ -136,44 +163,49 @@ function homePage(userName) {
         }
     }).start();
     return (<div>
-        <div id="Search" className={style.home_search_box}
-             style={{
-                 backgroundImage: "url(./src/images/homePageBackgrounds/" + random + ".png)", backgroundSize: "cover"
-             }}>
-            <div className={style.home_search_box_content}>
-                <div className={style.navbar_form}>
-                    <div className={style.home_search_icon}></div>
-                    <input ref={searchInfo} type="text" placeholder="Search" className={style.navbar_input}/>
-                    <ButtonGroup className={style.navbar_button_group}>
-                        <Button type={"submit"} className={style.navbar_button} onClick={search}>Search</Button>
-                    </ButtonGroup>
+        <div className={style.home_cover} ref={coverBox}>
+            <img className={style.home_cover_img} src="./src/images/loading.gif"></img>
+        </div>
+        <div ref={searchBox} style={{display: "none"}}>
+            <div id="Search" className={style.home_search_box}
+                 style={{
+                     backgroundImage: "url(./src/images/homePageBackgrounds/" + random + ".png)",
+                     backgroundSize: "cover"
+                 }}>
+                <div className={style.home_search_box_content}>
+                    <div className={style.navbar_form}>
+                        <div className={style.home_search_icon}></div>
+                        <input ref={searchInfo} type="text" placeholder="Search" className={style.navbar_input}/>
+                        <ButtonGroup className={style.navbar_button_group}>
+                            <Button type={"submit"} className={style.navbar_button} onClick={search}>Search</Button>
+                        </ButtonGroup>
+                    </div>
                 </div>
             </div>
-        </div>
-        <HomeCardGroup type={time} index={0}/>
-        <div className={style.home_interval}></div>
-        <HomeCardGroup type={"Sea Food"} index={1}/>
-        <div className={style.home_interval}></div>
-        <HomeCardGroup type={"Healthy"} index={2}/>
-        <div className={style.home_interval}></div>
-        <HomeCardGroup type={"Soup"} index={3}/>
-        <div className={style.home_interval}></div>
-        <HomeCardGroup type={"Salad"} index={4}/>
-        <div id="Bottom" className={style.home_bottom}>
-            {windowWidth > 1100 && <div className={style.home_bottom_icon}></div>}
-            <div className={style.home_bottom_title}>
-                <div className={style.home_bottom_title_icon}></div>
-                <div className={style.home_bottom_title_text}>Contact us</div>
-            </div>
-            <div className={style.home_bottom_content}>
-                <ul style={{listStyle: "none", padding: 0, color: "white", fontSize: "20px"}}>
-                    <li>Benjamin Goh: xxx@aucklanduni.ac.nz</li>
-                    <li>Cameron Nathan: xxx@aucklanduni.ac.nz</li>
-                    <li>Yasith Udagedara: xxx@aucklanduni.ac.nz</li>
-                    <li>Yunqi Zheng: yzhe583@aucklanduni.ac.nz</li>
-                    <li>Hok Lai Frankie Yu: xxx@aucklanduni.ac.nz</li>
-                    <li>Paul Lan: xxx@aucklanduni.ac.nz</li>
-                </ul>
+            {
+                data.map((item, index) =>
+                    <div key={index}>
+                        <HomeCardGroup data={item} index={index}/>
+                        <div className={style.home_interval}></div>
+                    </div>
+                )
+            }
+            <div id="Bottom" className={style.home_bottom}>
+                {windowWidth > 1100 && <div className={style.home_bottom_icon}></div>}
+                <div className={style.home_bottom_title}>
+                    <div className={style.home_bottom_title_icon}></div>
+                    <div className={style.home_bottom_title_text}>Contact us</div>
+                </div>
+                <div className={style.home_bottom_content}>
+                    <ul style={{listStyle: "none", padding: 0, color: "white", fontSize: "20px"}}>
+                        <li>Benjamin Goh: xxx@aucklanduni.ac.nz</li>
+                        <li>Cameron Nathan: xxx@aucklanduni.ac.nz</li>
+                        <li>Yasith Udagedara: xxx@aucklanduni.ac.nz</li>
+                        <li>Yunqi Zheng: yzhe583@aucklanduni.ac.nz</li>
+                        <li>Hok Lai Frankie Yu: xxx@aucklanduni.ac.nz</li>
+                        <li>Paul Lan: xxx@aucklanduni.ac.nz</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>);
