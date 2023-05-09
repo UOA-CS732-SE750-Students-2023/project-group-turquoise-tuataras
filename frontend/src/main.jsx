4b17857707407582060ec54d57aaa4d613dcf61e
hzyhzyq <53165831+hzyhzyq@users.noreply.github.com>
2023-05-10 01:16:03 +1200

    feat: home page remove restrict mode modify the icon

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { AuthContextProvider } from './context/AuthContext'
+import {AuthContextProvider} from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
     <AuthContextProvider>
-      <App />
+        <App/>
     </AuthContextProvider>
-  </React.StrictMode>,
 )
