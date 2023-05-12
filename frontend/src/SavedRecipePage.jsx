8a94aa42d845f7fd5ea5cb9d71571e773a5f1a2c
hzyhzyq <53165831+hzyhzyq@users.noreply.github.com>
2023-05-12 18:10:28 +1200

    feat: css on saved recipe

--- frontend/src/SavedRecipePage.jsx
+++ frontend/src/SavedRecipePage.jsx
@@ -1,79 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import SavedReceipeCard from './SavedReceipeCard';
-import { useAuthContext } from './hooks/useAuthContext';
+import {useAuthContext} from './hooks/useAuthContext';
 import styles from './SavedRecipePage.module.css';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default function SavedRecipePage() {
 
-  const { user, loading } = useAuthContext();
+    const {user, loading} = useAuthContext();
 
-  const [favoriteStatus, setFavoriteStatus] = useState(true);
+    const [favoriteStatus, setFavoriteStatus] = useState(true);
 
-  // Retrieve SavedRecipe Data ---- start
+    // Retrieve SavedRecipe Data ---- start
 
-  const [savedRecipes, setSavedRecipes] = useState(null);
+    const [savedRecipes, setSavedRecipes] = useState(null);
 
-  useEffect(() => {
-    async function fetchSavedRecipes() {
-      try {
-        const response = await axios.get(`${API_BASE_URL}/users/savedRecipes`, {
-          headers: {
-            Authorization: `Bearer ${user.token}`
-          }
-        })
-  
-        console.log(response.data)
-        setSavedRecipes(response.data);
-        console.log("savedRecipes = ", savedRecipes);
-        console.log("user = " ,user)
-        setFavoriteStatus(true);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-    fetchSavedRecipes();
-  }, [favoriteStatus]);
+    useEffect(() => {
+        async function fetchSavedRecipes() {
+            try {
+                const response = await axios.get(`${API_BASE_URL}/users/savedRecipes`, {
+                    headers: {
+                        Authorization: `Bearer ${user.token}`
+                    }
+                })
 
-  // Retrieve SavedRecipe Data ----- end
+                console.log(response.data)
+                setSavedRecipes(response.data);
+                console.log("savedRecipes = ", savedRecipes);
+                console.log("user = ", user)
+                setFavoriteStatus(true);
+            } catch (err) {
+                console.error(err);
+            }
+        }
 
-  
+        fetchSavedRecipes();
+    }, [favoriteStatus]);
 
-  return (
-    <div>
-      {(savedRecipes && user) ?
-        (<div>
-          <div className={styles.mainGrid}>
-            <div className={styles.productContainer}>
-              
-              {savedRecipes.map((recipe) => (
-                <SavedReceipeCard key={recipe.spoonacularId}
-                  item={recipe}
-                  favoriteStatus={favoriteStatus}
-                  setFavoriteStatus={setFavoriteStatus} />
-              ))}
-            </div>
-          </div>
-        </div>
-        ) : 
-        (
-          <LoadingImage savedRecipes = {savedRecipes} />
-        )}
-    </div>
-  );
+    // Retrieve SavedRecipe Data ----- end
 
-  function LoadingImage({savedRecipes}) {
-
-      let isSavedTecipe
 
     return (
+        <div>
+            {(savedRecipes && user) ?
+                (<div>
+                        <div className={styles.productContainer}>
+                            {savedRecipes.map((recipe) => (
+                                <SavedReceipeCard key={recipe.spoonacularId}
+                                                  item={recipe}
+                                                  favoriteStatus={favoriteStatus}
+                                                  setFavoriteStatus={setFavoriteStatus}/>
+                            ))}
+                        </div>
+                    </div>
+                ) :
+                (
+                    <LoadingImage savedRecipes={savedRecipes}/>
+                )}
+        </div>
+    );
+
+    function LoadingImage({savedRecipes}) {
+
+        let isSavedTecipe
+
+        return (
 
 
-
-
-        <img src="Loading_icon.gif" />
-    )
-}
+            <img src="Loading_icon.gif"/>
+        )
+    }
 }
